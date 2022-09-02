f66d07d3ccc1f46c342ab6bcc59bf9a3d1693f77
andijayawizard <andijayawizard@gmail.com>
2022-09-02 14:07:52 +0700

    0209

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  posts: any;
+  constructor(private service: PostService){}
+  ngOnInit(): void {
+      this.service.getPosts().subscribe(response=>{
+        this.posts=response;
+      });
+  }
   title = 'todo';
 }
